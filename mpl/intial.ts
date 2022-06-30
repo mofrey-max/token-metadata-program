26e1a11564b78d52cdb15238cc1fdbe535407128
mofrey-max <manugodfrey8@gmail.com>
2022-06-30 11:32:48 +0100

    update

--- mpl/intial.ts
+++ mpl/intial.ts
@@ -1,4 +1,3 @@
-
 import * as mpl from "@metaplex-foundation/mpl-token-metadata";
 import * as web3 from "@solana/web3.js";
 import * as anchor from '@project-serum/anchor';
@@ -9,18 +8,20 @@ export function loadWalletKey(keypairFile:string): web3.Keypair {
       new Uint8Array(JSON.parse(fs.readFileSync(keypairFile).toString())),
     );
     return loaded;
-  }
-
-const INITIALIZE = false;
+  } 
 
 async function main(){
-    console.log("let's name some tokens!");
-    const myKeypair = loadWalletKey("Your Keypair");
+
+    console.log("AGXT TOKEN NAME AND LOGO")
+    const myKeypair = loadWalletKey("Your Keypiar");
+    console.log(myKeypair.publicKey.toBase58());
     const mint = new web3.PublicKey("Your Token Address");
-    const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode("metadata"));
+    const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode('metadata'));
     const seed2 = Buffer.from(mpl.PROGRAM_ID.toBytes());
     const seed3 = Buffer.from(mint.toBytes());
+
     const [metadataPDA, _bump] = web3.PublicKey.findProgramAddressSync([seed1, seed2, seed3], mpl.PROGRAM_ID);
+   
     const accounts = {
         metadata: metadataPDA,
         mint,
@@ -38,32 +39,18 @@ async function main(){
         collection: null,
         uses: null
     }
-    let ix;
-    if (INITIALIZE) {
-        const args =  {
-            createMetadataAccountArgsV2: {
-                data: dataV2,
-                isMutable: true
-            }
-        };
-        ix = mpl.createCreateMetadataAccountV2Instruction(accounts, args);
-    } else {
-        const args =  {
-            updateMetadataAccountArgsV2: {
-                data: dataV2,
-                isMutable: true,
-                updateAuthority: myKeypair.publicKey,
-                primarySaleHappened: true
-            }
-        };
-        ix = mpl.createUpdateMetadataAccountV2Instruction(accounts, args)
-    }
+    const args =  {
+        createMetadataAccountArgsV2: {
+            data: dataV2,
+            isMutable: true
+        }
+    };
+    const ix = mpl.createCreateMetadataAccountV2Instruction(accounts, args);
     const tx = new web3.Transaction();
     tx.add(ix);
     const connection = new web3.Connection("https://api.mainnet-beta.solana.com");
     const txid = await web3.sendAndConfirmTransaction(connection, tx, [myKeypair]);
     console.log(txid);
-
 }
 
-main();
+main()
